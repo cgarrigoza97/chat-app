20489476c2b0e4ab5d239acc107febcd88f51abc
cgarrigoza97 <cgarrigoza@gmail.com>
2022-02-16 19:18:19 -0300

    finish sidebar header

--- src/components/SidebarHeader/SidebarHeader.tsx
+++ src/components/SidebarHeader/SidebarHeader.tsx
@@ -1,11 +1,24 @@
+import { CustomButton } from '../CustomButton/CustomButton'
 import { InputSearch } from '../InputSearch/InputSearch'
+import { IconContext } from 'react-icons';
+import { IoMenuOutline } from 'react-icons/io5'
 import styles from './SidebarHeader.module.css'
 
 export const SidebarHeader = () => {
+
+    const handleClick = () => {}
+
     return (
         <div className={styles.sidebarHeader}>
             <div className={styles.btnContainer}>
-
+                <CustomButton onClick={handleClick}>
+                    <IconContext.Provider value={{ 
+                        size: '1.8em',
+                        color: 'rgb(var(--gray-2))' 
+                    }}>
+                        <IoMenuOutline />
+                    </IconContext.Provider>
+                </CustomButton>
             </div>
 
             <div className={styles.inputContainer}>
