46f1356038df620c5e1590b867f20c1f6b92467c
cgarrigoza97 <cgarrigoza@gmail.com>
2022-03-08 23:15:17 -0300

    Start header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,23 @@
+import { IconButton } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 import styles from  './Header.module.css'
 
 
 export const Header = () => {
     return (
-        <div className={ styles.header }></div>
+        <div className={ styles.header }>
+            <div></div>
+
+            <div className={ styles.headerBtnContainer }>
+                <IconButton aria-label='search'>
+                    <SearchIcon />
+                </IconButton>
+                
+                <IconButton aria-label='more'>
+                    <MoreVertIcon />
+                </IconButton>
+            </div>
+        </div>
     )
 }
--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,8 +1,11 @@
+import { Header } from '../Header/Header'
 import styles from './MainContent.module.css'
 
 
 export const MainContent = () => {
     return (
-        <div className={ styles.mainContent }></div>
+        <div className={ styles.mainContent }>
+            <Header />
+        </div>
     )
 }
