eb5e5f0f59bf5a0cca8da44003d8f05c1f7b34ec
cgarrigoza97 <cgarrigoza@gmail.com>
2022-03-09 22:43:17 -0300

    Add header info

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,21 @@ import styles from  './Header.module.css'
 
 export const Header = () => {
     return (
-        <div className={ styles.header }>
-            <div></div>
+        <div className={styles.header}>
+            <div className={styles.headerInfoContainer}>
+                <div className={styles.headerInfoImgContainer}>
+                    <img src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' alt='icon' />
+                </div>
+
+                <div className={styles.headerInfoBodyContainer}>
+                    <div className={styles.headerInfoBodyHeader}>
+                        <h1>Chat Name</h1>
+                    </div>
+                    <div className={styles.headerInfoBodyContent}>
+                        <p>Placeholder for chat</p>
+                    </div>
+                </div>
+            </div>
 
             <div className={ styles.headerBtnContainer }>
                 <IconButton aria-label='search'>
